8479ade95bd0ec4de080cbc309e1c1b6c49b9c3e
Pravin Kandala <pravinkandala@hotmail.com>
2016-01-29 01:21:26 -0600

    working till getlocation()

--- ViewController.swift
+++ ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var updateButton: UIButton!
     var coreLocationManager = CLLocationManager()
+    var locationManager: LocationManager!
     
     
     override func viewDidLoad() {
@@ -23,11 +24,49 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         coreLocationManager.delegate = self
         
+        locationManager = LocationManager.sharedInstance
+        
         let authorizationCode = CLLocationManager.authorizationStatus()
         
-        if authorizationCode == CLAuthorizationStatus.NotDetermined
+        if authorizationCode == CLAuthorizationStatus.NotDetermined && coreLocationManager.respondsToSelector("requestAlwaysAuthorization") || coreLocationManager.respondsToSelector("requestWhenInUseAuthorization"){
+            if NSBundle.mainBundle().objectForInfoDictionaryKey("NSLocationAlwaysUsageDescription") != nil {
+                self.coreLocationManager.requestAlwaysAuthorization()
+            }
+            else {
+                print("No description provided")
+            }
+        }
+        else{
+            self.getLocation()
+        }
+        
+    }
+    
+    func getLocation(){
+        locationManager.startUpdatingLocationWithCompletionHandler { (latitude, longitude, status, verboseMessage, error) -> () in
+            self.displayLocation(CLLocation(latitude: latitude, longitude: longitude))
+        }
+    }
+    
+    func displayLocation(location:CLLocation){
+        mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude), span: MKCoordinateSpanMake(0.05, 0.05)), animated: true)
+        
+        let locationPinCoord = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.latitude)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationPinCoord
+        mapView.addAnnotation(annotation)
+        mapView.showAnnotations([annotation], animated: true)
+        
+        
     }
 
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status != CLAuthorizationStatus.NotDetermined || status != CLAuthorizationStatus.Denied || status != CLAuthorizationStatus.Restricted{
+            getLocation()
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
